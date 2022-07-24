102d45627282a5b6d774779c6c1522435a408b95
mo <mohibullah.kamal@gmail.com>
2022-07-24 17:27:51 +0600

    boolean exercise

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 //console.log("Hello World!!!")
 
-let a = 1
-let b = 2
-let c = 3
-console.log(a)
-console.log(b)
-console.log(c)
+// let a = 1
+// let b = 2
+// let c = 3
+// console.log(a)
+// console.log(b)
+// console.log(c)
+
+// let a = 1
+// let b = 2.435435
+// console.log(typeof a) //In JS its a number; in other programming lang its integer...
+// console.log(typeof b) //let us refer b as float... but in JS they are all type number...
+
+// let a = false
+// let b = true
+// console.log(typeof a)
+// console.log(typeof b)
