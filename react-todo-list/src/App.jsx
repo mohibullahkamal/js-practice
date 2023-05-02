cf30c9afba086a4f1a030360f150365bb60cddd5
mo <mohibullah.kamal@gmail.com>
2023-05-02 12:14:43 +0600

    add todo list with delete button

--- react-todo-list/src/App.jsx
+++ react-todo-list/src/App.jsx
@@ -1,19 +1,26 @@
 import "./styles.css"
 
 export default function App() {
-  return <form className="new-item-form">
+  return (
+    <>
+      <form className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          <input type="text" id="item" />
+        </div>
+        <button className="btn">ADD</button>
+      </form>
 
-    <div className="form-row">
-      <label htmlFor="item">New Item</label>
-      <input type="text" id="item" />
-    </div>
-    <button className="btn">ADD</button>
-
-    <div className="form-row">
-      <label htmlFor="item">Todo List</label>
-    </div>
-
-  </form>
+      <h1 className="header">Todo List</h1>
+      <ul className="list">
+        <li>
+          <label><input type="checkbox" />Item1</label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+      </ul>
+      
+    </>
+  )
 }
 
 
